2255649f216a051625d0c8a02071807b3d4b4ce0
emanoelvianna <emanoel.vianna@outlook.com>
2018-06-27 00:59:39 -0300

    master - versão final

--- app/bisection-service.js
+++ app/bisection-service.js
@@ -1,66 +1,77 @@
-(function() {
-    'use strict';
+var newEquation;
 
-    angular
-        .module('main')
-        .service('main.bisectionService', Service);
+function bisection(equation, a, b, tolerance) {
+    let epsilon = 0.00001;
+    let interaction = 0;
+    let midpoint = 0;
+    let results = [];
+    newEquation = equation;
 
-    Service.$inject = [];
+    while ((b - a) > epsilon) {
+        midpoint = math.eval((a + b) / 2);
+        let y_m = solve(midpoint);
+        let y_a = solve(a);
 
-    function Service() {
-        var self = this;
-
-        /* public method */
-        self.bisection = bisection;
-
-        function bisection(equation, a, b, tolerance) {
-            let epsilon = 0.00001;
-            let interaction = 0;
-            let midpoint = 0;
-            self.equation = equation;
-
-            while (interaction < tolerance) {
-                midpoint = math.eval((a + b) / 2);
-                let y_m = solve(midpoint);
-                let y_a = solve(a);
-
-                if ((y_m > 0 && y_a < 0) || (y_m < 0 && y_a > 0)) {
-                    b = midpoint;
-                } else {
-                    a = midpoint;
-                }
-                interaction++;
-                console.log("n:" + interaction + " ; a:" + a + "; b:" + b + " ; x:" + midpoint + " ; f(x):" + y_m);
-            }
-            draw();
-            console.log("- Solução aproximada: ");
-            return midpoint;
+        if ((y_m > 0 && y_a < 0) || (y_m < 0 && y_a > 0)) {
+            b = midpoint;
+        } else {
+            a = midpoint;
         }
+        interaction++;
 
-        function solve(value) {
-            let newEquation = self.equation.replace(/[xy]+/g, value);
-            return math.eval(newEquation);
+        let result = {
+            n: interaction,
+            a: a,
+            b: b,
+            x: midpoint,
+            f: y_m
         }
-
-        function draw() {
-            try {
-                const expr = math.compile(self.equation)
-
-                const xValues = math.range(-10, 10, 0.5).toArray()
-                const yValues = xValues.map(function(x) {
-                    return expr.eval({ x: x })
-                })
-
-                const trace1 = {
-                    x: xValues,
-                    y: yValues,
-                    type: 'scatter'
-                }
-                const data = [trace1]
-                Plotly.newPlot('plot', data)
-            } catch (err) {
-                console.error(err)
-            }
-        }
+        results.push(result);
     }
-}());
+    draw(results);
+    console.log("- Solução aproximada: ");
+    return midpoint;
+}
+
+function draw(results) {
+    let table = document.getElementById("myTable");
+    table.innerHTML = "";
+    let row;
+
+    results.reverse();
+    results.filter(function(result) {
+        row = table.insertRow(0);
+        row.insertCell(0).appendChild(document.createTextNode(result.n));
+        row.insertCell(1).appendChild(document.createTextNode(result.a));
+        row.insertCell(2).appendChild(document.createTextNode(result.b));
+        row.insertCell(3).appendChild(document.createTextNode(result.f));
+    });
+    row = table.insertRow(0);
+    row.insertCell(0).appendChild(document.createTextNode("n"));
+    row.insertCell(1).appendChild(document.createTextNode("a"));
+    row.insertCell(2).appendChild(document.createTextNode("b"));
+    row.insertCell(3).appendChild(document.createTextNode("f(x)"));
+
+    try {
+        const expr = math.compile(newEquation)
+
+        const xValues = math.range(-10, 10, 0.5).toArray()
+        const yValues = xValues.map(function(x) {
+            return expr.eval({ x: x })
+        })
+
+        const trace1 = {
+            x: xValues,
+            y: yValues,
+            type: 'scatter'
+        }
+        const data = [trace1]
+        Plotly.newPlot('plot', data)
+    } catch (err) {
+        console.error(err)
+    }
+}
+
+function solve(value) {
+    return math.eval(newEquation.replace(/[xy]+/g, value));
+}
